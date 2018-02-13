8d9a1bc73677ce24bba125dc9db7da3f65e22835
ORCL <sliver47@126.com>
2018-02-13 17:27:33 +0800

    Rye

--- day1/for.py
+++ day1/for.py
@@ -9,9 +9,21 @@
 # continue：跳出本次循环，进入到下次循环
 #   break ：结束整个循环
 
+# continue例子：
+"""
 for i in range(0, 10):
-    if i < 5:
+    if i < 3:
         print("loop", i)
     else:
         continue
     print("hehe...")
+"""
+
+# break例子：大循环与小循环的嵌套
+
+for i in range(10):
+    print("-----------", i)
+    for j in range(10):
+        print(j)
+        if j > 5:
+            break
